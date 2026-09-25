61598c4a971ad3d1f08bf5c8d2df697b08cc1425
jha-nishant <jha.nishant0428@gmail.com>
2026-09-25 22:32:16 +0530

    Add: pow-x-n - submission-3

---
+++ Data Structures & Algorithms/pow-x-n/submission-3.py
@@ -0,0 +1,23 @@
+import math
+
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        # n can be 0 and negative
+        if n == 0:
+            return 1
+        num = self.myPowPositive(x, abs(n))
+        if n < 0:
+            return 1 / num
+        return num
+    
+    def myPowPositive(self, x: float, n: int) -> float:
+        if n == 1:
+            return x
+        half = int(n / 2)
+        odd = n % 2
+        half_power = self.myPow(x, half)
+        out = half_power * half_power
+        if odd:
+            out *= x
+        
+        return out
