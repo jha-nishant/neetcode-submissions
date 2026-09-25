85d71d57a04b881971945e1f20d682814abfc1aa
jha-nishant <jha.nishant0428@gmail.com>
2026-09-25 17:50:07 +0530

    Add: non-cyclical-number - submission-0

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-0.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        seen = set()
+        start = n
+        while start not in seen:
+            if start == 1:
+                break
+            seen.add(start)
+            start = self.digitSqSum(start)
+
+        return start == 1
+    
+    def digitSqSum(self, n: int) -> int:
+        sqSum = 0
+        while n > 0:
+            rem = n % 10
+            sqSum += rem * rem
+            n = int(n / 10)
+        return sqSum
